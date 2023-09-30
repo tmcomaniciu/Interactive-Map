e3c432afd0b2dcc42d4e40f893557cdbba89ea59
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-09-30 14:04:29 -0400

    added marker to map

--- index.js
+++ index.js
@@ -1,66 +1,58 @@
-//TODO: get user location then find taco trucks near them, not random
-//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
-async function getCoords() {
-  //user might reject or accept or take 5min to see that i asked them to give permission
-  //dont return promise resolve inline
-  let pos = await new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-
-  //make it a little less accurate so not invasive of user privacy by rounding away digits
-  return [Math.floor(pos.coords.latitude), Math.floor(pos.coords.longitude)];
-}
-function createMap(coords, tacoTrucks) {
-  //Create map
-
-  var map = L.map("map").setView(coords, 13);
-  //Load tiles
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
-  //Add markers
-  tacoTrucks.forEach((t) => createMarker(t, map));
-}
-
-function createMarker(t, map) {
-  //map was undefined but their code didn't throw ref error
-  // console.log("whats wrong", t, map)
-  var marker = L.marker([t.lat, t.long]).bindPopup(t.name).addTo(map);
-}
-
-//get tacos from api
-let tacos_api_base_url = "https://60d23844858b410017b2d60b.mockapi.io/tacos";
-async function getTacoTrucks() {
-  let response = await fetch(tacos_api_base_url);
-  let data = await response.json();
-  return data;
-}
-
-async function searchTrucks(searchTerm) {
-  //load some data from an api
-  //hard to make this global because it is loaded in promise
-  let tacoTrucks = await getTacoTrucks();
-  // console.log(tacoTrucks)
-  //look inside each truck name does contain searchTerm?
-  if (searchTerm === undefined || searchTerm === "") {
-    return tacoTrucks;
+//triggers browser location popup
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
   }
-  return tacoTrucks.filter((t) => t.name.includes(searchTerm.toLowerCase()));
 }
+//setting map and current location variables
+let map;
+let currentPosition;
+// function that shows current position on a marker
+function showPosition(position) {
+  currentPosition = [position.coords.latitude, position.coords.longitude];
+  map = L.map("map").setView(currentPosition, 13);
 
-//entry point of our code
-async function main() {
-  let tacoTrucks = await searchTrucks();
-  console.log(tacoTrucks);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 
-  //get coords
-  let coords = await getCoords();
-  console.log(coords);
-  //add map to screen
-  createMap(coords, tacoTrucks);
+  L.marker(currentPosition)
+    .addTo(map)
+    .bindPopup("here <i>we</i> <strong>are</strong>")
+    .openPopup();
 }
+//event listener that grabs location data for our specific businesses using the FourSquare API
+document.getElementById("submit").addEventListener("click", async (event) => {
+  event.preventDefault();
+  let business = document.getElementById("business").value;
+  console.log(business);
+  console.log(currentPosition);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "fsq3fymS/i3YrLU6I5Fqkw9PE5SzEO1IO7GY4EzPHU/Y1T0=",
+    },
+  };
 
-//execute main
-main();
+  fetch(
+    `https://api.foursquare.com/v3/places/search?query=${business}&ll=${currentPosition[0]}%2C${currentPosition[1]}&limit=5`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => displayData(response))
+    .catch((err) => console.error(err));
+});
+// displays the data our API found for us
+function displayData(response) {
+  for (let i = 0; i < response.results.length; i++) {
+    let b = response.results[i];
+    L.marker([b.geocodes.main.latitude, b.geocodes.main.longitude])
+      .addTo(map)
+      .bindPopup(`${b.name}<br />${b.location.address}`)
+      .openPopup();
+  }
+}
+//invokes getLocation function
+getLocation();
